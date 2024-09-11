9aa93331d824e1208be9f71ed5230e810ba83b0a
joshuaharris07 <joshua.harris07@gmail.com>
2024-09-11 13:29:49 -0600

    Fleshed out the functions and menu function. Added some formatting to menu for better readability.

--- todo_list.py
+++ todo_list.py
@@ -1,28 +1,72 @@
-def add_task(task_name, status = "incomplete"):
-    pass
+def add_task(task_name, status = "Incomplete"):
+    if len(task_name.strip()) < 1:
+        print("No task was entered, returning to the menu.")
+    elif [task_name, status] in task_list:
+        print(f"That task is already in your to-do list and is currently {status}.")
+    else:
+        new_task = [task_name, status]
+        task_list.append(new_task)
 
 def view_tasks():
-    pass
+    for task in task_list:
+        print(f"{task[0]} is {task[1]}")
 
-def complete_task():
-    pass
+def complete_task(completed_task):
+    try:
+        task_list.remove([completed_task, "Incomplete"])
+    except:
+        print(f"{completed_task} is not in your Todo list.")
+    else:
+        print(f"{completed_task} has been marked complete.")
+        finished_tasks.append(completed_task)
+    finally:
+        print("Returning to the menu.")
 
-def delete_task():
-    pass
+def delete_task(task_to_delete):
+    try:
+        task_list.remove([task_to_delete, "Incomplete"])
+    except:
+        print(f"{task_to_delete} is not in your Todo list.")
+    else:
+        print(f"{task_to_delete} has been deleted from the Todo list.")
+    finally:
+        print("Returning to the menu.")
 
-print("Welcome to the To-Do List App!")
+def view_finished_tasks():
+    print("Here is your list of tasks you have completed:")
+    for task in finished_tasks:
+        print(f"{task}")
+
+task_list = [["Water plants", "Incomplete"], ["Eating", "Incomplete"], ["Read a book", "Incomplete"]] #empty before finalizing? maybe leave
+finished_tasks = ["Shopping"]
+
+print("Welcome to the To-Do List App!\n")
+
 while True:
-    menu_action = input("Menu:\n1. Add a task\n2. View tasks\n3. Mark a task as complete\n4. Delete a task\n5. Quit\n")
+    welcome = "Menu"
+    print(welcome.center(20, "*"))
+    menu_action = input("\n1. Add a task\n2. View tasks\n3. Mark a task as complete\n4. Delete a task\n5. View completed tasks\n6. Quit\n")
     if menu_action == "1":
-        add_task()
+        task_name = input("Please input a name for the task: ").capitalize()
+        add_task(task_name)
     elif menu_action == "2":
         view_tasks()
     elif menu_action == "3":
-        complete_task()
+        completed_task = input("Which task have you completed: ").capitalize()
+        if len(completed_task.strip()) < 1:
+            print("No task was entered, returning to the menu.")
+        else:
+            complete_task(completed_task)
     elif menu_action == "4":
-        delete_task()
+        task_to_delete = input("Which task would you like to delete? Please type the name: ").capitalize()
+        if len(task_to_delete.strip()) < 1:
+            print("No task was entered, returning to the menu.")
+        else:
+            delete_task(task_to_delete)
     elif menu_action == "5":
+        view_finished_tasks()
+    elif menu_action == "6":
         print("Thank you for using the To-Do List App! Have a great day!")
         break
     else:
-        print("Invalid input. Please enter 1, 2, 3, 4, or 5 to make your selection.")
+        print("Invalid input. Please enter 1, 2, 3, 4, or 5 to make your selection.\n")
